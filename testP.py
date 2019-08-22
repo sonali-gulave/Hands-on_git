2b21a02fbcfcfadec63ab06b92856969820d2986
Sonali Gulave <sonaligulave9@gmail.com>
2019-08-22 17:34:12 +0530

    changed this file with new practice code

--- testP.py
+++ testP.py
@@ -1,1 +1,6 @@
-print "sonali"
+def f(arg1,arg2,kwarg1="kw1",kwarg2='kw2',*args,**kwargs):
+    print('arg:{0},arg2:{1}''kwarg1:{2},kwarg2:{3}'.format(arg1,arg2,kwarg1,kwarg2))
+    if args:
+        print('args: ',str(args))
+    if kwargs:
+        print('kwargs: ',kwargs)
